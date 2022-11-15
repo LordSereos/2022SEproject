206e27ef3cda0a784494f129a725cb72c0fd396b
petrelis <vistinas108@gmail.com>
2022-11-15 18:24:37 +0200

    Added comments to FormatPdf

--- SolarPanel/src/main/java/FormatPdf.java
+++ SolarPanel/src/main/java/FormatPdf.java
@@ -2,9 +2,6 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
-import java.io.IOException;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 import java.io.FileOutputStream;
 
 public class FormatPdf {
@@ -13,14 +10,20 @@ public class FormatPdf {
     }
 
     public void PrintPdf() {
+        //Set Local Directory
         String LOCAL_DIRECTORY = System.getProperty("user.dir");
+        //pdf file path
         String FILE_NAME = LOCAL_DIRECTORY + "\\SolarPanel\\PdfDocs\\doc.pdf";
+        //Image path
         String IMAGE_PATH = LOCAL_DIRECTORY+ "\\SolarPanel\\Images\\testimage.png";
         Document document = new Document();
         try {
+            //Create a pdfwriter to the pdf file
             PdfWriter.getInstance(document, new FileOutputStream(new File(FILE_NAME)));
+            //open pdf document
             document.open();
 
+            //Create and add paragraph
             Font font = new Font();
             font.setSize(20);
             Paragraph p = new Paragraph("", font);
@@ -28,13 +31,17 @@ public class FormatPdf {
             p.setAlignment(Element.ALIGN_CENTER);
             document.add(p);
 
+            //Add image
             document.add(Image.getInstance(IMAGE_PATH));
 
+            //Create paragraph
             Paragraph p2 = new Paragraph("Test Text wowowowow\n" +
                     "woooooooooow", font);
 
+            //Add paragraph
             document.add(p2);
 
+            //close document
             document.close();
             System.out.println("Done!");
         }
