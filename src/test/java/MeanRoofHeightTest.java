81e5aa5a6886c822e6c4759226de51577503d8ff
Arnas <63949706+arnasce@users.noreply.github.com>
2022-11-22 21:39:03 +0200

    Merged team2 and team4 branches

--- src/test/java/MeanRoofHeightTest.java
+++ src/test/java/MeanRoofHeightTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MeanRoofHeightTest {
--- src/test/java/RectangleCoordinatesTest.java
+++ src/test/java/RectangleCoordinatesTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleCoordinatesTest {
